f5e2612c94bf98263a510597dacfcbad8f14dc9a
usmansbk <usmansbk@gmail.com>
2023-01-13 00:30:14 +0100

    nit

--- src/components/GoogleButton.tsx
+++ src/components/GoogleButton.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Button } from "react-native-paper";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function GoogleButton() {
   return (
     <Button
-      icon={() => <Icon name="google" size={24} />}
       mode="contained"
       buttonColor="#4285F4"
       textColor="#ffffff"
