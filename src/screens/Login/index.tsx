ced8f79b7f27c475c02e9d7aa0e676874fa7183b
usmansbk <usmansbk@gmail.com>
2023-01-13 14:18:00 +0100

    add copywrite

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { ScrollView, View } from "react-native";
+import { Text } from "react-native-paper";
 import GoogleButton from "~components/GoogleButton";
 import styles from "./styles";
 
 export default function Login() {
+  const { t } = useTranslation();
   return (
     <ScrollView contentContainerStyle={styles.contentContainer}>
-      <View style={styles.body} />
+      <View style={styles.body}>
+        <Text variant="displaySmall">{t("login.pre")}</Text>
+        <Text variant="displayLarge">{t("login.lectures")}</Text>
+        <Text variant="displayLarge">{t("login.tests")}</Text>
+        <Text variant="displayLarge">{t("login.exams")}</Text>
+        <Text variant="displaySmall">{t("login.post")}</Text>
+      </View>
       <GoogleButton />
     </ScrollView>
   );
