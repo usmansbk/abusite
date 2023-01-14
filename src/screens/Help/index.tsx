6ad4a247d0da28e20c7eb4010d27f9438f7fde39
usmansbk <usmansbk@gmail.com>
2023-01-14 18:45:55 +0100

    add help items

--- src/screens/Help/index.tsx
+++ src/screens/Help/index.tsx
@@ -1,6 +1,43 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import {useTranslation} from 'react-i18next';
+import {FlatList} from 'react-native';
+import {Divider, List} from 'react-native-paper';
 import Container from '~components/Container';
 
 export default function Help() {
-  return <Container />;
+  const {t} = useTranslation();
+
+  const items = useMemo(
+    () => [
+      {
+        id: 'terms',
+        label: t('help.items.terms'),
+      },
+      {
+        id: 'privacy',
+        label: t('help.items.privacy'),
+      },
+      {
+        id: 'review',
+        label: t('help.items.review'),
+      },
+      {
+        id: 'support',
+        label: t('help.items.support'),
+      },
+    ],
+    [],
+  );
+
+  return (
+    <Container>
+      <FlatList
+        data={items}
+        ItemSeparatorComponent={Divider}
+        renderItem={({item: {label}}) => (
+          <List.Item title={label} onPress={() => null} />
+        )}
+      />
+    </Container>
+  );
 }
