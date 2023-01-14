a18e2d90adc3800981ac71f137c3125e6f5ffe63
usmansbk <usmansbk@gmail.com>
2023-01-15 00:16:32 +0100

    nit

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {ScrollView, View} from 'react-native';
-import {Button, Divider, TextInput} from 'react-native-paper';
+import {Button, Divider, List, TextInput} from 'react-native-paper';
 import UserAvatar from '~components/UserAvatar';
 import Container from '~components/Container';
 import useMe from '~hooks/api/useMe';
+import useLogout from '~hooks/useLogout';
 import styles from './styles';
 
 export default function Profile() {
   const {me} = useMe();
-  const {firstName, lastName, picture} = me!;
+  const logout = useLogout();
+
+  const {firstName, lastName, picture, email} = me!;
 
   return (
     <Container>
@@ -31,11 +34,28 @@ export default function Profile() {
             value={lastName}
             style={styles.gap}
           />
-          <Button style={styles.gap} mode="contained-tonal">
+          <TextInput
+            mode="outlined"
+            label="Email"
+            editable={false}
+            value={email}
+            style={styles.gap}
+          />
+          <Button disabled style={styles.gap} mode="contained">
             Save
           </Button>
         </View>
         <Divider />
+        <List.Item
+          left={props => <List.Icon {...props} icon="log-out" />}
+          title="Logout"
+          onPress={logout}
+        />
+        <List.Item
+          left={props => <List.Icon {...props} icon="trash" />}
+          title="Deactivate account"
+          onPress={logout}
+        />
       </ScrollView>
     </Container>
   );
