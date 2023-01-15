adf8a1171dec2f187fbb46e0f8a45024000a42f3
usmansbk <usmansbk@gmail.com>
2023-01-15 13:07:34 +0100

    add schema validation

--- src/screens/Profile/UpdateProfileForm.tsx
+++ src/screens/Profile/UpdateProfileForm.tsx
@@ -1,7 +1,9 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useMemo} from 'react';
 import {View} from 'react-native';
-import {TextInput, Button} from 'react-native-paper';
+import {TextInput, Button, HelperText} from 'react-native-paper';
 import {useForm, Controller} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import {UpdateUserProfileInput, User} from '~graphql/__generated__/graphql';
 import useUpdateProfile from '~hooks/api/useUpdateProfile';
 import UploadUserAvatar from './UploadUserAvatar';
@@ -15,12 +17,34 @@ export default function UpdateProfileForm({user}: Props) {
   const {firstName, lastName, picture, email} = user;
   const {loading, update} = useUpdateProfile();
 
+  const schema = useMemo(
+    () =>
+      yup
+        .object<UpdateUserProfileInput>({
+          firstName: yup
+            .string()
+            .trim()
+            .max(100, () => 'Name too long')
+            .required(() => "What's your first name?"),
+          lastName: yup
+            .string()
+            .trim()
+            .max(100, () => 'Name too long')
+            .required(() => "What's your last name?"),
+        })
+        .noUnknown()
+        .required(),
+    [],
+  );
+
   const {
     control,
     handleSubmit,
     reset,
     formState: {isDirty, touchedFields, errors},
-  } = useForm<UpdateUserProfileInput>();
+  } = useForm<UpdateUserProfileInput>({
+    resolver: yupResolver(schema),
+  });
 
   useEffect(() => {
     reset({
@@ -38,34 +62,50 @@ export default function UpdateProfileForm({user}: Props) {
         control={control}
         name="firstName"
         render={({field: {value, onChange, onBlur}}) => (
-          <TextInput
-            autoComplete="name-given"
-            mode="outlined"
-            label="First name"
-            value={value}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            style={styles.gap}
-            error={Boolean(
-              touchedFields.firstName && errors.firstName?.message,
+          <>
+            <TextInput
+              autoComplete="name-given"
+              mode="outlined"
+              label="First name"
+              value={value}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              style={styles.gap}
+              error={Boolean(
+                touchedFields.firstName && errors.firstName?.message,
+              )}
+            />
+            {Boolean(touchedFields.firstName && errors.firstName?.message) && (
+              <HelperText type="error">
+                {errors.firstName?.message as string}
+              </HelperText>
             )}
-          />
+          </>
         )}
       />
       <Controller
         name="lastName"
         control={control}
         render={({field: {value, onChange, onBlur}}) => (
-          <TextInput
-            autoComplete="name-family"
-            mode="outlined"
-            label="Last name"
-            value={value}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            style={styles.gap}
-            error={Boolean(touchedFields.lastName && errors.lastName?.message)}
-          />
+          <>
+            <TextInput
+              autoComplete="name-family"
+              mode="outlined"
+              label="Last name"
+              value={value}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              style={styles.gap}
+              error={Boolean(
+                touchedFields.lastName && errors.lastName?.message,
+              )}
+            />
+            {Boolean(touchedFields.lastName && errors.lastName?.message) && (
+              <HelperText type="error">
+                {errors.lastName?.message as string}
+              </HelperText>
+            )}
+          </>
         )}
       />
       <TextInput
