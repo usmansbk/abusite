f25cd633825015b9d3c2e20c104b0aa72fb9462a
usmansbk <usmansbk@gmail.com>
2023-01-22 01:17:20 +0100

    nit

--- src/screens/NewTimetable/index.tsx
+++ src/screens/NewTimetable/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Container from '~components/Container';
 import TimetableForm from '~components/TimetableForm';
 import useCreateTimetable from '~hooks/api/useCreateTimetable';
+import {RootStackScreenProps} from '~types';
 
-export default function NewTimetable() {
-  const {loading, handleCreateTimetable} = useCreateTimetable();
+export default function NewTimetable({
+  navigation,
+}: RootStackScreenProps<'NewTimetable'>) {
+  const {loading, handleCreateTimetable, timetable} = useCreateTimetable();
+
+  useEffect(() => {
+    if (timetable) {
+      navigation.pop();
+    }
+  }, [timetable]);
 
   return (
     <Container>
